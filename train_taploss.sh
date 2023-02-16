ffe4f2b9df3b28f6e3172151317d528ebbe2c207
Kawon Lee <91329965+famouskawon@users.noreply.github.com>
2023-02-16 18:30:55 -0500

    Update train_taploss.sh

--- train_taploss.sh
+++ train_taploss.sh
@@ -27,7 +27,7 @@ then
   echo $cmd
   eval $cmd
 
-  cmd="python3 content/TAPLoss-master/Demucs/denoiser/train.py dummy=waveform+1_pho_seg_ac_loss   continue_pretrained='dns64'   dset=custom_dns   acoustic_loss=True   acoustic_loss_only=False   phoneme_segmented=False   stft_loss=True   ac_loss_weight=${acoustic_weight}   stft_loss_weight=${stft_weight}   epochs=${epochs}   num_workers=2   batch_size=${batch_size}   ddp=${num_gpus}"
+  cmd="python3 /content/TAPLoss-master/Demucs/denoiser/train.py dummy=waveform+1_pho_seg_ac_loss   continue_pretrained='dns64'   dset=custom_dns   acoustic_loss=True   acoustic_loss_only=False   phoneme_segmented=False   stft_loss=True   ac_loss_weight=${acoustic_weight}   stft_loss_weight=${stft_weight}   epochs=${epochs}   num_workers=2   batch_size=${batch_size}   ddp=${num_gpus}"
   echo $cmd
   eval $cmd
 
